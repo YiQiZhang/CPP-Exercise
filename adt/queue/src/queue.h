4cce55e2cf0cd7e50eea9ba5398dec64430f3adc
YiQiZhang <jerry_techtree@126.com>
2015-09-25 09:37:57 +0800

    Updated 搞了个队列,还没测试
    Signed-off-by: YiQiZhang <jerry_techtree@126.com>

---
+++ adt/queue/src/queue.h
@@ -0,0 +1,67 @@
+#include <memory>
+#include "error.h"
+
+using std::unique_ptr;
+
+#define MIN_QUEUE_SIZE (5)
+
+template <typename T>
+class queue{
+private:
+	size_t maxSize;
+	size_t size;
+	size_t front;
+	size_t rear;
+	unique_ptr<T[]> datas;
+	// 下标回环
+	size_t circle(size_t index)
+	{
+		if (++index > maxSize) {
+			index = 0;
+		}
+
+		return index;
+	}
+public:
+	queue(size_t s = MIN_QUEUE_SIZE) : 
+		maxSize(s),
+		size(0),
+		front(1),
+		rear(0) 
+	{
+		if(maxSize < MIN_QUEUE_SIZE) {
+			error("queue too small");
+		}
+		datas.reset(new T[maxSize]);
+	}
+	void enqueue(const T &ele)
+	{
+		if (isFull()) {
+			error("queue is full, fail to enqueue");
+		}
+		rear = circle(rear);
+		++size;
+		datas[rear] = ele;
+	}
+	T dequeue()
+	{
+		if (isEmpty())
+			error("queue is empty, fail to dequeue");
+		T res = datas[front];
+		--size;
+		front = circle(front);
+
+		return res;
+	}
+	bool isEmpty() const
+	{
+		return size <= 0 ? true : false;
+	}
+	bool isFull() const
+	{
+		return maxSize >= size ? true : false;
+	}
+	~queue() {
+		datas.release();
+	}
+};
---
+++ adt/queue/src/testQueue.cpp
@@ -0,0 +1,26 @@
+// testQueue.cpp
+#include <iostream>
+#include <string>
+#include "queue.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+struct task{
+	string name;
+	task() : name("undefine") {}
+	task(const string &n : name(n) {}
+};
+
+void testQueue()
+{
+
+}
+
+int main(int argc, char **argv)
+{
+	testQueue();
+
+	return 0;
+}
