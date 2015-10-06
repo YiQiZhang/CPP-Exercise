980d4be2c9c2ff33e3a67e39b80b34408026ada0
YiQiZhang <jerry_techtree@126.com>
2015-10-06 09:37:29 +0800

    UPDATED 初始化一下AVL平衡树
    Signed-off-by: YiQiZhang <jerry_techtree@126.com>

---
+++ adt/tree/src/avlTree.h
@@ -0,0 +1,13 @@
+//
+//  avlTree.h
+//
+//  Created by Jerry on 10/6/15.
+//  Copyright (c) 2015 Jerry. All rights reserved.
+//
+
+#ifndef AVLTREE_H
+#define AVLTREE_H
+
+
+
+#endif;
---
+++ adt/tree/src/testAvlTree.cpp
@@ -0,0 +1,7 @@
+//
+//  testAvlTree.h
+//
+//  Created by Jerry on 10/6/15.
+//  Copyright (c) 2015 Jerry. All rights reserved.
+//
+
