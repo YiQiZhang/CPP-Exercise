5a3b17a7a2e050f9036b6403ed76a6b087f70cb8
YiQiZhang <jerry_techtree@126.com>
2015-10-14 11:48:21 +0800

    Updated 伸展树建了两个文件，还有B-Tree的实现没有
    Signed-off-by: YiQiZhang <jerry_techtree@126.com>

---
+++ adt/tree/src/splayTree.h
@@ -0,0 +1,7 @@
+//
+//  splayTree.h
+//
+//  Created by Jerry on 10/11/15.
+//  Copyright (c) 2015 Jerry. All rights reserved.
+//
+
---
+++ adt/tree/src/testSplayTree.cpp
@@ -0,0 +1,17 @@
+//
+//  testSplayTree.cpp
+//
+//  Created by Jerry on 10/11/15.
+//  Copyright (c) 2015 Jerry. All rights reserved.
+//
+
+void testSplayTree()
+{
+
+}
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
